ddc85a85eb94e35c77a1c0bd2bf1320c4b58612a
Jason Holmes <holmes@squareup.com>
2017-09-02 17:38:37 -0700

    Don't need MQTT callbacks

--- src/MQTTHandler.cpp
+++ src/MQTTHandler.cpp
@@ -5,24 +5,13 @@ MQTTHandler::MQTTHandler(const PubSubClient &pubSubClient) : client(pubSubClient
   // We're initialized!
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
- Serial.print("Message arrived [");
- Serial.print(topic);
- Serial.print("] ");
-}
-
 void MQTTHandler::setup(MQTTConfig config) {
   Serial.printf("MQTT Configured: %s\n", config.mqtt_server);
 
-  client.setServer("192.168.1.5", 1883)
-        .setCallback(callback);
+  client.setServer("192.168.1.5", 1883);
 
   if (!client.connect("IRRECVR-PUTINAREALID", "openhab", "mqtt@165")) {
     Serial.println("Connecting failed!");
-  }
-
-  if (!client.subscribe("test/topic")) {
-    Serial.println("Subscribing Failed!");
   }
 
   if (!client.publish("test/topic", "diditwork")) {
